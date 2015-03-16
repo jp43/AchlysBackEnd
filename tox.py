9b10a1d1bc3412090f9c24a0623d2eddd3e59a31
Philip Winter <pwinter@ualberta.ca>
2015-03-16 17:42:07 -0600

    Fix path in tox.py

--- tox.py
+++ tox.py
@@ -281,7 +281,7 @@ def do_md(lig_id, rec_id, pose_path):
     shutil.copyfile('%s/amber/leap.in' % PARAMDIR, '%s/leap.in' % md_work_dir)
     shutil.copyfile('%s/namd/quick_min.conf' % PARAMDIR, '%s/min.conf' % md_work_dir)
     shutil.copyfile('%s/namd/quick_heat.conf' % PARAMDIR, '%s/heat.conf' % md_work_dir)
-    shutil.copyfile('%s/namd/quick_eq.conf' % PARAMDIR, '%s/eq.conf' % md_work_dir)
+    shutil.copyfile('%s/namd/quick_equ.conf' % PARAMDIR, '%s/eqU.conf' % md_work_dir)
     dock_work_dir = '%s/dock_lig%d_rec%d' % (WORKDIR, lig_id, rec_id)
     shutil.copyfile('%s/lig.pdb' % dock_work_dir, '%s/lig.pdb' % md_work_dir)
     shutil.copyfile('%s/complex.pdb' % dock_work_dir, '%s/complex.pdb' % md_work_dir)
